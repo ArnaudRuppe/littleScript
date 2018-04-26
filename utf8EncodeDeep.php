ae261fcc8348c879317416eb493c013fb3f92754
Ruppe Arnaud <arnaud.ruppe@gmail.com>
2018-04-26 18:55:56 +0200

    Add files via upload

---
+++ utf8EncodeDeep.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+ * @param input - Array, string or object to convert into utf8
+ * @return void
+*/
+function utf8_encode_deep($input) {
+	if (is_string($input)) {
+		$input = utf8_encode($input);
+	} else if (is_array($input)) {
+		foreach ($input as &$value) {
+			utf8_encode_deep($value);
+		}
+		unset($value);
+	} else if (is_object($input)) {
+		$vars = array_keys(get_object_vars($input));
+		foreach ($vars as $var) {
+			utf8_encode_deep($input->$var);
+		}
+	}
+}
